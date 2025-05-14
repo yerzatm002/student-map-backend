f0cb6b5d59254d09196e3d96cd47092750fe50c6
yerzatm002 <erzatnis@gmail.com>
2025-05-15 02:45:51 +0500

    fixing bag on the app

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
+import os
 from flask import Flask
-from config import Config
-from models import db
 from dotenv import load_dotenv
 from flask_cors import CORS
-import os
+from config import Config
+from models import db
 
 load_dotenv()
-print("POSTGRES:", os.getenv("DB_HOST"), os.getenv("DB_PORT"))
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db.init_app(app)
-CORS(app) 
+CORS(app)
 
-with app.app_context():
-    db.create_all()
-
+# === Регистрация маршрутов ===
 from routes.students import students_bp
 from routes.regions import regions_bp
 from routes.geo import geo_bp
 from routes.stats import stats_bp
 from routes.reference import reference_bp
+
 app.register_blueprint(reference_bp)
 app.register_blueprint(stats_bp)
 app.register_blueprint(students_bp)
 app.register_blueprint(regions_bp)
 app.register_blueprint(geo_bp)
 
-# === Запуск приложения ===
+# === Запуск приложения (Render ищет PORT) ===
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_ENV") != "production")
